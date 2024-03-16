8b9a6a84027e0dffa2f1487abfd6f1866557bc3f
sumiren <sumiren.and.t@gmail.com>
2024-03-16 23:08:07 +0900

    chore: layout to block

--- frontend/src/app/architecture-test/page.tsx
+++ frontend/src/app/architecture-test/page.tsx
@@ -5,7 +5,7 @@ export const runtime = 'edge'
 
 export default async function Home() {
   return (
-    <main className="p-24">
+    <main className="p-24 min-h-screen block">
       <h1 className="text-3xl">Next.js App Router + Streaming + Partial Prerendering</h1>
       <Suspense fallback={<p className="mt-16 text-xl">fetching on Next.js server side...</p>}>
         <ArchitectureTest/>
